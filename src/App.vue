15555b9a0fb48e26a353d06ad9407fca2ff86fad
japsermalik <jaspermalik729@gmail.com>
2020-08-10 17:37:13 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Neo />
+</template>
+
+<script>
+import Neo from "./components/Neo.vue";
+
+export default {
+  name: "App",
+  components: {
+    Neo,
+  },
+};
+</script>
