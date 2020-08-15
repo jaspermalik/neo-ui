102035773aeb9f1f18e390226eaa95422033025a
japsermalik <jaspermalik729@gmail.com>
2020-08-15 21:52:59 +0800

    hover之后右滚

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    Switch实例
+    <Switch />
   </div>
 </template>
+<script lang="ts">
+import Switch from "../lib/Switch.vue";
+export default {
+  components: { Switch },
+};
+</script>
