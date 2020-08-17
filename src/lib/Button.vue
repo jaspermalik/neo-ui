09cc8aa9b23923539d7968059370f794146aa651
japsermalik <jaspermalik729@gmail.com>
2020-08-17 16:36:59 +0800

    添加Button基础样式

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -17,4 +17,39 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #1b68fa;
+$radius: 4px;
+.neo-button {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background-color: #fff;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: all 250ms;
+  & + & {
+    margin-left: 8px;
+  }
+  &:hover,
+  &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
+  &:focus {
+    outline: none;
+  }
+  &::-moz-focus-inner {
+    border: 0;
+  }
+}
 </style>
