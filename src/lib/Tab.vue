ca5851b732cd4d8aa062004cde2d3a061ccd4c25
japsermalik <jaspermalik729@gmail.com>
2020-09-24 21:40:12 +0800

    创建Tabs和Tab组件

---
+++ src/lib/Tab.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/lib/Tabs.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Tabs组件
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
