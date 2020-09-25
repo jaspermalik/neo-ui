cf339c611a9d2c9623e104816ec515aa174734c5
japsermalik <jaspermalik729@gmail.com>
2020-09-25 22:33:40 +0800

    添加dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
     Dialog实例
+    <h1>示例1</h1>
+    <Dialog></Dialog>
   </div>
 </template>
+
+<script lang="ts">
+import Dialog from "../lib/Dialog.vue";
+export default {
+  components: { Dialog },
+};
+</script>
